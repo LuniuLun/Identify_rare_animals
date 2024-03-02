4cb58de6c457aaa0810bbb6b05a4cb37eda946b0
DucVan <nguyenducvan260903@gmail.com>
2024-03-02 22:29:25 +0700

    [ADD] add avatar no user

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -54,31 +54,37 @@ function Home() {
                     <img className={cx("img-animal")} src="/img/con_cong.jpg" alt="" />
                     <span className={cx("name-animal")}>Pavo muticus imperator</span>
                     <span className={cx("time-upload")}>Mar 24</span>
+                    <img src="/img/no-user-img.jpg" alt="" className={cx("img-user")}/>
                 </div>
                 <div className={cx("card")}>
                     <img className={cx("img-animal")} src="/img/con_cong.jpg" alt="" />
                     <span className={cx("name-animal")}>Pavo muticus imperator</span>
                     <span className={cx("time-upload")}>Mar 24</span>
+                    <img src="/img/no-user-img.jpg" alt="" className={cx("img-user")}/>
                 </div>
                 <div className={cx("card")}>
                     <img className={cx("img-animal")} src="/img/con_cong.jpg" alt="" />
                     <span className={cx("name-animal")}>Pavo muticus imperator</span>
                     <span className={cx("time-upload")}>Mar 24</span>
+                    <img src="/img/no-user-img.jpg" alt="" className={cx("img-user")}/>
                 </div>
                 <div className={cx("card")}>
                     <img className={cx("img-animal")} src="/img/con_cong.jpg" alt="" />
                     <span className={cx("name-animal")}>Pavo muticus imperator</span>
                     <span className={cx("time-upload")}>Mar 24</span>
+                    <img src="/img/no-user-img.jpg" alt="" className={cx("img-user")}/>
                 </div>
                 <div className={cx("card")}>
                     <img className={cx("img-animal")} src="/img/con_cong.jpg" alt="" />
                     <span className={cx("name-animal")}>Pavo muticus imperator</span>
                     <span className={cx("time-upload")}>Mar 24</span>
+                    <img src="/img/no-user-img.jpg" alt="" className={cx("img-user")}/>
                 </div>
                 <div className={cx("card")}>
                     <img className={cx("img-animal")} src="/img/con_cong.jpg" alt="" />
                     <span className={cx("name-animal")}>Pavo muticus imperator</span>
                     <span className={cx("time-upload")}>Mar 24</span>
+                    <img src="/img/no-user-img.jpg" alt="" className={cx("img-user")}/>
                 </div>
             </div>
         </div>
