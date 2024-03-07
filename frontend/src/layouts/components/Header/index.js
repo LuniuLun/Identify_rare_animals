66c1d1a5f86ef6b31d040a782f5d7a99c527d3c8
DucVan <nguyenducvan260903@gmail.com>
2024-03-08 00:27:20 +0700

    [UPDATE] update a little header UI when logging in  successfully

--- frontend/src/layouts/components/Header/index.js
+++ frontend/src/layouts/components/Header/index.js
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import LogoSVG from "../../../assets/logo";
+import { Fragment, useState } from "react";
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [isLogin, setIsLogin] = useState(true);
+    const [showListOptionsUser, setShowListOptionsUser] = useState(false);
+
+    const setOpenOptions = () => {
+        setShowListOptionsUser((preivous) => !preivous);
+    };
     return (
         <div className={cx("wrapper")}>
             <div className={cx("right-item")}>
@@ -18,6 +25,13 @@ function Header() {
                     <Link className={cx("link")} to={"/"}>
                         <span>Explore</span>
                     </Link>
+                    {isLogin === true ? (
+                        <Link className={cx("link")} to={"/"}>
+                            <span>Your Observations</span>
+                        </Link>
+                    ) : (
+                        <></>
+                    )}
                     <Link className={cx("link")} to={"/"}>
                         <span>Community</span>
                         <FontAwesomeIcon className={cx("down-icon")} icon={faChevronDown} />
@@ -29,9 +43,34 @@ function Header() {
                 </div>
             </div>
             <div className={cx("left-item")}>
-                <Link className={cx("link")} to={"/login"}>
-                    Log In
-                </Link>
+                {isLogin === true ? (
+                    <Fragment>
+                        <img src="/img/no-user-img.jpg" alt="" className={cx("ava-user")} onClick={setOpenOptions} />
+                        <FontAwesomeIcon className={cx("down-icon")} icon={faChevronDown} onFocus={setOpenOptions} />
+                        {showListOptionsUser === true ? (
+                            <div className={cx("list-options-user")}>
+                                <Link to={"/Profile"} className={cx("option-user")}>
+                                    Profile
+                                </Link>
+                                <Link to={"/"} className={cx("option-user")}>
+                                    Your Observations
+                                </Link>
+                                <Link to={"/upload-animal"} className={cx("option-user")}>
+                                    Uploads
+                                </Link>
+                                <Link to={"/login"} className={cx("option-user")}>
+                                    Sign out
+                                </Link>
+                            </div>
+                        ) : (
+                            <></>
+                        )}
+                    </Fragment>
+                ) : (
+                    <Link className={cx("link")} to={"/login"}>
+                        Log In
+                    </Link>
+                )}
             </div>
         </div>
     );
