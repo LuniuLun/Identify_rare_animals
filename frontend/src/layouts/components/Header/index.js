974c0cf54e015572e4cb60e29f503a2eb4cb6664
DucVan <nguyenducvan260903@gmail.com>
2024-03-09 16:51:20 +0700

    [ADD] add drop and drag image

--- frontend/src/layouts/components/Header/index.js
+++ frontend/src/layouts/components/Header/index.js
@@ -3,23 +3,40 @@ import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-
 import { faCalendarDays, faLocationDot, faXmark } from "@fortawesome/free-solid-svg-icons";
 import LogoSVG from "../../../assets/logo";
 import { Fragment, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
 const cx = classNames.bind(styles);
 
 function Header() {
     const [isLogin, setIsLogin] = useState(true);
     const [showListOptionsUser, setShowListOptionsUser] = useState(false);
     const [showModal, setShowModal] = useState(false);
+    const [isHavingImage, setIsHavingImage] = useState(false);
+    const [yourImage, setImage] = useState([]);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        accept: "image/*",
+        onDrop: (acceptedFiles) => {
+            setImage(
+                acceptedFiles.map((upFile) =>
+                    Object.assign(upFile, {
+                        preview: URL.createObjectURL(upFile),
+                    })
+                )
+            );
+            setIsHavingImage(true);
+        },
+    });
+
     const closeModal = () => {
         setShowModal(false);
     };
-    const openModal = ()=> {
-        
+    const openModal = () => {
         setShowModal(true);
-    }
+    };
 
     const setOpenOptions = () => {
         setShowListOptionsUser((preivous) => !preivous);
@@ -55,7 +72,9 @@ function Header() {
             <div className={cx("left-item")}>
                 {isLogin === true ? (
                     <Fragment>
-                        <button className={cx("btn_addAnimal")} onClick={openModal}>Upload</button>
+                        <button className={cx("btn_addAnimal")} onClick={openModal}>
+                            Upload
+                        </button>
                         <img src="/img/no-user-img.jpg" alt="" className={cx("ava-user")} onClick={setOpenOptions} />
                         <FontAwesomeIcon className={cx("down-icon")} icon={faChevronDown} onFocus={setOpenOptions} />
                         {showListOptionsUser === true ? (
@@ -88,25 +107,41 @@ function Header() {
                 <div className={cx("add-animal-modal")}>
                     <div className={cx("body")}>
                         <FontAwesomeIcon icon={faXmark} className={cx("close-icon")} onClick={closeModal} />
-                        <div className={cx("form-detail")}>
-                            <div className={cx("title")}>Editing observation:</div>
-                            <div className={cx("information")}>
-                                <FontAwesomeIcon icon={faMagnifyingGlass} className={cx("icon")} />
-                                <input className={cx("species-name")} placeholder="Species name" />
+
+                        {isHavingImage ? (
+                            <div className={cx("wrapper-detail-animal")}>
+                                <div className={cx("form-detail")}>
+                                    <div className={cx("title")}>Editing observation:</div>
+                                    <div className={cx("information")}>
+                                        <FontAwesomeIcon icon={faMagnifyingGlass} className={cx("icon")} />
+                                        <input className={cx("species-name")} placeholder="Species name" />
+                                    </div>
+                                    <div className={cx("information")}>
+                                        <FontAwesomeIcon icon={faCalendarDays} className={cx("icon")} />
+                                        <input className={cx("date")} placeholder="Date" />
+                                    </div>
+                                    <div className={cx("information")}>
+                                        <FontAwesomeIcon icon={faLocationDot} className={cx("icon")} />
+                                        <input className={cx("location")} placeholder="Location" />
+                                    </div>
+                                    <div className={cx("information")}>
+                                        <textarea className={cx("note")} placeholder="Note"></textarea>
+                                    </div>
+                                </div>
+                                <img src={yourImage[0].preview} alt="" className={cx("animal-image")} />
                             </div>
-                            <div className={cx("information")}>
-                                <FontAwesomeIcon icon={faCalendarDays} className={cx("icon")} />
-                                <input className={cx("date")} placeholder="Date" />
+                        ) : (
+                            <div className={cx("add-image")}>
+                                <div {...getRootProps()}>
+                                    <input {...getInputProps()} />
+                                    {isDragActive ? (
+                                        <p className={cx("introduction")}>Drop the image here...</p>
+                                    ) : (
+                                        <p className={cx("introduction")}>Drag & drop image here or click to select image</p>
+                                    )}
+                                </div>
                             </div>
-                            <div className={cx("information")}>
-                                <FontAwesomeIcon icon={faLocationDot} className={cx("icon")} />
-                                <input className={cx("location")} placeholder="Location" />
-                            </div>
-                            <div className={cx("information")}>
-                                <textarea className={cx("note")} placeholder="Note"></textarea>
-                            </div>
-                        </div>
-                        <img src="/img/con_cong.jpg" alt="" className={cx("animal-image")} />
+                        )}
                     </div>
                 </div>
             ) : (
