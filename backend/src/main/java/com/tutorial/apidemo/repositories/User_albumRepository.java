93a22e9e2078f4d12ca73a40126ee56bd640ddaf
DucVan <nguyenducvan260903@gmail.com>
2024-04-02 01:07:16 +0700

    [FIX] fix User_albumRepository

--- backend/src/main/java/com/tutorial/apidemo/repositories/User_albumRepository.java
+++ backend/src/main/java/com/tutorial/apidemo/repositories/User_albumRepository.java
@@ -1,22 +1,20 @@
 package com.tutorial.apidemo.repositories;
 
 import com.tutorial.apidemo.models.User_album;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-
-import java.util.List;
-
-public interface User_albumRepository extends JpaRepository<User_album, Long> {
-    List<User_album> findByiDUserAnimal(Integer iDUserAnimal);
-=======
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface User_albumRepository extends JpaRepository<User_album, Long> {
     @Transactional
     @Modifying
     @Query(value = "INSERT INTO user_album (IDUserAnimal, ImageLink) VALUES (?1, ?2)", nativeQuery = true)
     void saveUser_album(Integer iDUserAnimal, String imageLink);
->>>>>>> main
+
+    List<User_album> findByiDUserAnimal(Integer iDUserAnimal);
 }
