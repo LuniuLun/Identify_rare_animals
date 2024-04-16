25570de86480659b4488ca3ac7083f857df751b3
DucVan <nguyenducvan260903@gmail.com>
2024-04-16 17:58:52 +0700

    [UPDATE] loading component

--- frontend/src/components/Loading/index.js
+++ frontend/src/components/Loading/index.js
@@ -3,10 +3,14 @@ import classNames from "classnames/bind";
 import styles from "./loading.module.scss";
 const cx = classNames.bind(styles);
 
-function Loading() {
+function Loading({messsage}) {
     return (
         <div className={cx("wrapper")}>
-            <img src="/loading_animal.svg" alt="Loading Animal" className={cx("loading_image")} />
+            <div className={cx("loading_bar")}>
+                <img src="/loading_animal.svg" alt="Loading Animal" className={cx("loading_image")} />
+                <div className={cx("loading_progress")} />
+            </div>
+            <span className={cx("message")}>{messsage}</span>
         </div>
     );
 }
