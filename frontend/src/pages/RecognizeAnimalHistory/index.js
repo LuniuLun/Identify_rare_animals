812b7271c0eb8d447a891d0b47f7e5117a0f28ed
hyynee04 <huyblue2504@gmail.com>
2024-04-23 21:44:21 +0700

    xong recognizeanimalhistory

--- frontend/src/pages/RecognizeAnimalHistory/index.js
+++ frontend/src/pages/RecognizeAnimalHistory/index.js
@@ -1,63 +1,37 @@
 import classNames from "classnames/bind";
 import styles from "./RecognizeAnimalHistory.module.scss"
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faCircleRight } from "@fortawesome/free-solid-svg-icons";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import {
-    faCalendarDays,
-    faLocationDot,
-    faXmark,
-    faObjectGroup,
-    faMagnifyingGlassLocation,
-    faAngleRight,
-    faAngleLeft,
-} from "@fortawesome/free-solid-svg-icons";
-import axios from "axios";
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
+import DetailAnimal from "../../components/DetailAnimal";
 
 const cx = classNames.bind(styles);
 
 function RecognizeAnimalHistory() {
+    const [animalScientificName, setAnimalScientificName] = useState("");
+    const [showDetailAnimal, setShowDetailAnimal] = useState(false);
+
     return ( 
         <div className={cx("wrapper")}>
             <div className={cx("head")}>
-                <span>Observation History</span>
+                <span>Lịch Sử Quan Sát</span>
                 <div className={cx("right-items")}>
                     <div className={cx("wrapper-find-animal")}>
                         <FontAwesomeIcon icon={faMagnifyingGlass} className={cx("find-icon")} />
                         <input className={cx("name-animal")} placeholder="Animal" />
                     </div>
-                    <button className={cx("btn_find")}>Go</button>
+                    <button className={cx("btn_find")}>Tìm</button>
                 </div>
             </div>
             <div className={cx("statistic")}></div>
             <div className={cx("body")}>
                 <div className={cx("wrapper-detail-animal")}>
-                    <button className={cx("form-detail")}>
-                        <div className={cx("wrapper-image")}>
-                            <div className={cx("slide-images")}>
-                                <img className={cx("animal-image")} src="/img/con_rua.png"/>
-                            </div>
-                        </div>
-                        <div>
-                            <div className={cx("information")}>
-                                <span className={cx("species-name")}><b>CÚ LỢN RỪNG PHƯƠNG ĐÔNG</b></span>
-                            </div>
-                            <div className={cx("information")}>
-                                <span className={cx("species-name")}>Phodilus badius</span>
-                            </div>
-                            <div className={cx("information")}>
-                                <span className={cx("date")}>4-25-2023</span>
-                            </div>
-                            <div className={cx("information")}>                        
-                                <span className={cx("location")}>Asia</span>
-                            </div>
-                            <div className={cx("information")}>                        
-                                <button className={cx("delete_but")}>Delete</button>
-                            </div>
-                        </div>
-                    </button>
-
-                    <button className={cx("form-detail")}>
+                <button 
+                        className={cx("form-detail")} 
+                        onClick={() => {
+                            setShowDetailAnimal(true);
+                        }}
+                    >
                         <div className={cx("wrapper-image")}>
                             <div className={cx("slide-images")}>
                                 <img className={cx("animal-image")} src="/img/con_cong.jpg"/>
@@ -74,13 +48,31 @@ function RecognizeAnimalHistory() {
                                 <span className={cx("date")}>4-25-2023</span>
                             </div>
                             <div className={cx("information")}>                        
-                                <span className={cx("location")}>Asia</span>
+                                <span className={cx("location")}>Châu Á</span>
                             </div>
                             <div className={cx("information")}>                        
-                                <button className={cx("delete_but")}>Delete</button>
+                                <button className={cx("delete_but")}>Xóa</button>
                             </div>
                         </div>
                     </button>
+                {showDetailAnimal === true ? (
+                    <div className={cx("show-detail-animal")}>
+                        <div className={cx("detail-animal")}>
+                            <FontAwesomeIcon 
+                                icon={faXmark} 
+                                className={cx("close-icon")} 
+                                onClick={() => {
+                                    setShowDetailAnimal(false);
+                                }}
+                            />
+                            <DetailAnimal animalScientificName={animalScientificName} />
+                        </div>
+                    </div>
+                ) : (
+                    <div></div>
+                )}
+
+                    
                 </div>
             </div>
         </div> 
